9a4d7fdf496d7e97e653be6577e2e7f2f8a35720
lhsharing <daley.luis@gmail.com>
2019-05-23 17:23:30 -0400

    +devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+#   devise_scope :users do
+#     get 'login', to: 'devise/sessions#new'
+#   end
+#  devise_scope :users do
+#     get 'logout', to: 'devise/sessions#destroy'
+#   end
+
+
+
+
+
+
+
+
+
+
 end
