0ab47dbd0ce685c5de40566282b38c6f48f64c07
lhsharing <daley.luis@gmail.com>
2019-05-23 19:35:18 -0400

    +root|resources for all models (excl user)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+ root 'recording_sessions#index'
+ 
+   resources  :studios, :recording_sessions, :engineers, :artists
 
 
 #   devise_scope :users do
