f8d274969393cf4f6ff55b00b68bfa6453d5f1be
lhsharing <daley.luis@gmail.com>
2019-05-24 17:56:36 -0400

    artist now  accepts_nested_attributes_for

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,6 +3,7 @@ class Artist < ApplicationRecord
     has_many :recording_sessions #adds methods to my model
     has_many :engineers, through: :recording_sessions
     has_many :studios, through: :recording_sessions
+    accepts_nested_attributes_for :recording_sessions
 
 validates :name, presence: true, length: { minimum: 1 }
 validates :name, uniqueness: true
