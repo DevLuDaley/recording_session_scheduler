96d17d76e8561555e84b6b40ed3199c2afa3c273
lhsharing <daley.luis@gmail.com>
2019-05-24 17:56:57 -0400

    studio/engineer now  accepts_nested_attributes_for

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,9 +1,10 @@
 class Engineer < ApplicationRecord
 
     has_many :recording_sessions #adds methods to my model
-    has_many :artists, through: :recording_session
-    has_many :studios, through: :recording_session
-
+    has_many :artists, through: :recording_sessions
+    has_many :studios, through: :recording_sessions
+    accepts_nested_attributes_for :recording_sessions
+    
 validates :name, presence: true, length: { minimum: 1 }
 validates :name, uniqueness: true
 
--- app/models/studio.rb
+++ app/models/studio.rb
@@ -3,6 +3,7 @@ class Studio < ApplicationRecord
     has_many :recording_sessions #adds methods to my model
     has_many :engineers, through: :recording_sessions
     has_many :artists, through: :recording_sessions
+    accepts_nested_attributes_for :recording_sessions
 
 validates :name, presence: true, length: { minimum: 1 }
 validates :name, uniqueness: true
