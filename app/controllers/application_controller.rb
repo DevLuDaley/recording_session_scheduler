6fbd919002556898bce1a8691335e62251b89cac
lhsharing <daley.luis@gmail.com>
2019-05-23 23:09:58 -0400

    +before_action :authenticate_user! to app ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_user!
+#      protected
+#   def authenticate_user!
+#     if user_signed_in?
+#       super
+#     else
+#       redirect_to login_path, :notice => 'if you want to add a notice'
+#       ## if you want render 404 page
+#       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+#     end
+ # end
 end
