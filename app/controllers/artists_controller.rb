f3fd441e4bace814e2502cdb3325400178794be7
lhsharing <daley.luis@gmail.com>
2019-05-23 23:10:26 -0400

    +authenticate_user! to artist/eng ctrls

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,4 @@
 class ArtistsController < ApplicationController
+    # before_filter :authenticate_user!
+    before_action :authenticate_user!
 end
--- app/controllers/engineers_controller.rb
+++ app/controllers/engineers_controller.rb
@@ -1,2 +1,4 @@
 class EngineersController < ApplicationController
+    # before_filter :authenticate_user!
+    before_action :authenticate_user!
 end
