1d3a9c4269665013d0bd1e218fe1003690ae3983
lhsharing <daley.luis@gmail.com>
2019-05-24 17:51:14 -0400

    +logic to engineersCTRL

--- app/controllers/engineers_controller.rb
+++ app/controllers/engineers_controller.rb
@@ -1,4 +1,17 @@
 class EngineersController < ApplicationController
     # before_filter :authenticate_user!
     before_action :authenticate_user!
+
+
+    def index
+    @engineers = Engineer.all
+    end
+
+def show
+    index
+    # binding.pry
+    @engineer = Engineer.find(params[:id])
+    #     redirect_to action: 'index'
+  end
+
 end
