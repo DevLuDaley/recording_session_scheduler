c8908be571cb885a1edaafd9beb1f964a904c158
lhsharing <daley.luis@gmail.com>
2019-05-24 20:26:49 -0400

    + actions to studios CTRL

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -2,41 +2,70 @@ class StudiosController < ApplicationController
     # before_filter :authenticate_user!
   before_action :authenticate_user!
 
-
 def index
-    @engineers = Engineer.all
     @studios = Studio.all
+    @engineers = Engineer.all
     @artists = Artist.all
-    if params[:artist_id]
-      @recording_sessions = Artist.find(params[:artist_id]).recording_sessions
-    elsif params[:engineer_id]
-      @recording_sessions = Engineer.find(params[:engineer_id]).recording_sessions
-    elsif params[:studio_id]
-      @recording_sessions = Studio.find(params[:studio_id]).recording_sessions
-    else
-      @recording_sessions = RecordingSession.all
+     @recording_sessions = RecordingSession.all
    #    redirect_to action: 'index'
  end
- end
 
+def new
+    @studio = Studio.new
+  end
 
-
+def create
+      # binding.pry
+      @studio = Studio.new(studio_params)
+  #    @studio = Studio.new(params[:name]
+      #index
+      # @studio = Artist.find(params[:id])
+      if @studio.save
+        # raise params.inspect
+  #      redirect_to action: 'index' # , notice: 'Artist created'
+        redirect_to @studio # , notice: 'Artist created'
+      else
+        # @studios = Studio.all
+        render action: 'new'
+      end
+  end
 
     def edit
-    #index
-    @studios = Studio.all
+      #      binding.pry
+      @studio = Studio.find(params[:id])
+#    index
+    #@studios = Studio.all
     #@engineer = Engineer.find(params[:id])
     #@artist = Artist.find(params[:id])
     #@recording_session = RecordingSession.find(params[:id])
-    @studio =Studio.find(params[:id])
   end
 
-
 def show
-  index
+  #index
     # binding.pry
     @studio = Studio.find(params[:id])
     #     redirect_to action: 'index'
 
 end
+
+def update
+   # index
+    #binding.pry
+    @studio = Studio.find(params[:id])
+    # raise params.inspect
+    if @studio.update(params.require(:studio).permit(:name, :location))
+      redirect_to studio_path(@studio)
+    else
+      render 'edit'
+    end
+end
+#end
+private
+  def studio_params
+    params.require(:studio).permit(:name, :location)
+  end
+
+
+
+#end
 end
