65f0fa0c1c25803e5a6853a07151ed6de83affcc
lhsharing <daley.luis@gmail.com>
2019-05-25 10:36:59 -0400

    + accepts nested attrs to artist.rb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,7 @@ class Artist < ApplicationRecord
     has_many :engineers, through: :recording_sessions
     has_many :studios, through: :recording_sessions
     accepts_nested_attributes_for :recording_sessions
+    accepts_nested_attributes_for :engineers
 
 validates :name, presence: true, length: { minimum: 1 }
 validates :name, uniqueness: true
