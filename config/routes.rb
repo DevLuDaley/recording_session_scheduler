d2496c664979def9e4ab81addeefa17a69d0bd91
lhsharing <daley.luis@gmail.com>
2019-06-03 08:57:59 -0400

    routes.rb file => removed unused code

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "callbacks", registrations: "registrations"}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  root 'recording_sessions#index'
- 
    resources  :studios, :recording_sessions, :engineers, :artists, :comments
-#resources :photos, :only => [:index, :show]
-  # resources :studios do
-  #   resources :recording_sessions#, :only => [:create, :show]
-  # end
-  # resources :engineers do
-  #   resources :recording_sessions#, :only => [:index, :show, :new, :edit]
-  # end
-  resources :artists do #, #shallow: true do 
-    resources :comments#, :only => [:index, :show, :new, :edit]
-  end
 
-  
-  resources :artists do #, #shallow: true do 
-    resources :recording_sessions#, :only => [:index, :show, :new, :edit]
-  end
+   resources :artists do #, #shallow: true do
+      resources :comments#, :only => [:index, :show, :new, :edit]
+   end
 
-
-#   devise_scope :users do
-#     get 'login', to: 'devise/sessions#new'
-#   end
-#  devise_scope :users do
-#     get 'logout', to: 'devise/sessions#destroy'
-#   end
-
+    resources :artists do #, #shallow: true do
+      resources :recording_sessions#, :only => [:index, :show, :new, :edit]
+    end
 end
