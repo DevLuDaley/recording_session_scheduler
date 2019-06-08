5fa62bc48ae66f801825667aaad5d5449dabc153
lhsharing <daley.luis@gmail.com>
2019-06-08 16:47:07 -0400

    comment + serial gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,4 @@ class Comment < ApplicationRecord
   belongs_to :artist
   validates :artist_id, presence: true, length: { minimum: 1 }
 
-  
 end
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :artist_id
+  attributes :id, :content, :artist_id, :created_at
   belongs_to  :artist
 end
